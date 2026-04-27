e16c3f5459f6e3c99254649486df5a7c45997a47
CZhang5688 <CodyZhang5688@gmail.com>
2026-04-27 20:09:50 +0100

    created method to initialise the agent class

--- uu24101_Part_2.py
+++ uu24101_Part_2.py
@@ -2,3 +2,25 @@
 import numpy as np
 import matplotlib as plt
 
+class Agent:
+    # infectious agent in the SEIR monte carlo model,
+    # this stores the position and its compartment
+
+    # using numbers to represent each state
+    EMPTY = 0
+    SUSCEPTIBLE = 1
+    EXPOSED = 2 
+    INFECTED = 3
+    RECOVERED = 4
+
+    # method to initialise and create a new Agent.
+    def __init__(self, position, state):
+        self.xpos = position[0]
+        self.ypos = position[1]
+        self.state = state
+
+    
+    
+
+
+
