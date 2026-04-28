1e1edbe0252244afd638bb8810212a8422dee013
CZhang5688 <CodyZhang5688@gmail.com>
2026-04-28 21:43:08 +0100

    Removing the unnecessary monte carlo function from part 1

--- uu24101_Part_1.py
+++ uu24101_Part_1.py
@@ -3,15 +3,6 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-# creating the monte carlo simulation function 
-def monte(myfunc, xmin, xmax, N):
-    samples = np.random.uniform(low=xmin, high=xmax, size=N)
-    values = myfunc(samples)
-    mean = values.sum()/N
-    meansq = (values*values).sum()/N
-    integral = (xmax - xmin) * mean
-    error = (xmax - xmin) * np.sqrt((meansq - mean * mean) / N)
-    return (integral, error)
 
 # PART 1
 # creating the function to solve the coupled set of equations (7) - (10)
