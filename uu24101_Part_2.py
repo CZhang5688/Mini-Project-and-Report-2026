260ca08acf8ab30b2b3b295684580ddb7fe7583b
CZhang5688 <CodyZhang5688@gmail.com>
2026-04-28 13:13:51 +0100

    created the move, infected_neighbour and update_state method

--- uu24101_Part_2.py
+++ uu24101_Part_2.py
@@ -1,6 +1,6 @@
 # importing necessary libraries
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 class Agent:
     # infectious agent in the SEIR monte carlo model,
@@ -19,8 +19,81 @@ class Agent:
         self.ypos = position[1]
         self.state = state
 
+    # method to allow agents to move to neighbouring lattice cell.
+    # if occupied, agent remains at original cell. 
+    def move(self, lattice, rng):
+        numlattice = lattice.shape[0]
+
+        # moving the agent in a random direction to a neighbouring cell
+        directions = [(1,0), (0,1), (-1,0), (0, -1)]
+        dx, dy = directions[rng.integers(0, 4)]
+
+        # updating posiiton of the agent
+        newx = self.xpos + dx
+        newy = self.ypos + dy
+        
+        # since I'm using a periodic boundary 
+        # I've used the modulus operator to handle edge cases
+        newx = newx % numlattice
+        newy = newy % numlattice
+
+        if lattice[newx, newy] == Agent.EMPTY:
+            lattice[self.xpos, self.ypos] = Agent.EMPTY
+
+            self.xpos = newx
+            self.ypos = newy
+
+            lattice[self.xpos, self.ypos] = self.state
+
+    def infected_neighbour(self, lattice):
+        
+        # method to check if neighbours are infected agents. 
+
+        numlattice = lattice.shape[0]
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        for dx, dy in directions:
+            nx = self.xpos + dx
+            ny = self.ypos + dy
+
+            nx = nx % numlattice
+            ny = ny % numlattice
+
+            if lattice[nx, ny] == Agent.INFECTED:
+                return True
+
+        return False
     
+    def update_state(self, lattice, rng, beta, sigma, gamma):
+        # method to update the SEIR states using SEIR probabilities specified
+
+        old_state = self.state
+
+        if self.state == Agent.SUSCEPTIBLE:
+            if self.infected_neighbour(lattice):
+                if rng.random() < beta:
+                    self.state = Agent.EXPOSED
+
+        elif self.state == Agent.EXPOSED:
+            if rng.random() < sigma:
+                self.state = Agent.INFECTED
+
+        elif self.state == Agent.INFECTED:
+            if rng.random() < gamma:
+                self.state = Agent.RECOVERED
+
+        if self.state != old_state:
+            lattice[self.xpos, self.ypos] = self.state
+
+
+
+
     
+   
+
+
+
 
 
 
